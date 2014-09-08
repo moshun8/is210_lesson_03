3947ba4d4661a99d15dc6b8fda4f3e8dea40b8da
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-08 09:22:56 -0400

    Raw input with strings and integers

---
+++ task_01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Adding raw input"""
+
+QUESTION_01 = raw_input("What's your favorite band? : ")
+QUESTION_02 = int(raw_input("What's 1 + 2? : "))
+
