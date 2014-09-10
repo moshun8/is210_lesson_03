fe1b0a4b6f2156a4bc91da1e586aff473fbc98f4
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-10 08:32:25 -0400

    Asking questions

--- task_01.py
+++ task_01.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Adding raw input"""
+"""Adding raw input."""
 
 QUESTION_01 = raw_input("What's your favorite band? : ")
 QUESTION_02 = int(raw_input("What's 1 + 2? : "))
