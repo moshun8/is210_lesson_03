843430a05f281d219ad9a2c26747222a4b3faee0
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-10 10:13:51 -0400

    Conditionals and raw input

---
+++ task_02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Conditionals with raw_input and blood pressure"""
+
+BP_STATUS = int(raw_input(
+	"What is your blood pressure? : "))
+
+if BP_STATUS <= 90:
+	BP_STATUS = "Low"
+elif BP_STATUS > 90 and BP_STATUS <= 119:
+	BP_STATUS = "Ideal"
+elif BP_STATUS > 119 and BP_STATUS <= 139:
+	BP_STATUS = "Warning"
+elif BP_STATUS > 139 and BP_STATUS <= 160:
+	BP_STATUS = "High"
+elif BP_STATUS > 160:
+	BP_STATUS = "Emergency"
+
+print "Your status is {0}.".format(BP_STATUS)
