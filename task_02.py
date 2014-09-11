8a30fea3f0c8ac22767faa3a96573df9ca77dcec
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-11 09:18:45 -0400

    Conditions and raw input

--- task_02.py
+++ task_02.py
@@ -6,14 +6,14 @@ BP_STATUS = int(raw_input(
 	"What is your blood pressure? : "))
 
 if BP_STATUS <= 90:
-	BP_STATUS = "Low"
+    BP_STATUS = "Low"
 elif BP_STATUS > 90 and BP_STATUS <= 119:
-	BP_STATUS = "Ideal"
+    BP_STATUS = "Ideal"
 elif BP_STATUS > 119 and BP_STATUS <= 139:
-	BP_STATUS = "Warning"
+    BP_STATUS = "Warning"
 elif BP_STATUS > 139 and BP_STATUS <= 160:
-	BP_STATUS = "High"
+    BP_STATUS = "High"
 elif BP_STATUS > 160:
-	BP_STATUS = "Emergency"
+    BP_STATUS = "Emergency"
 
 print "Your status is {0}.".format(BP_STATUS)
