f187a6e62dc735dd0543422b9b9f642116849154
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-09-11 10:52:08 -0400

    Loan Report

--- task_05.py
+++ task_05.py
@@ -7,58 +7,43 @@ from decimal import Decimal
 NAME = raw_input("What is your name? ")
 PRINCIPLE = int(raw_input(
     'What is the amount of your principle (amount being borrowed)? '))
+YEARS = int(raw_input("For how many years is this loan being borrowed? "))
+QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
 
 if PRINCIPLE <= 199999:
-    YEARS = int(raw_input(
-    'For how many years is this loan being borrowed? '))
     if 1 <= YEARS < 15:
-    	QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
         if QUALIFIED == "Yes":
             INTEREST = Decimal('.0363')
         elif QUALIFIED == "No":
             INTEREST = Decimal('.0465')
     elif 15 <= YEARS < 20:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
         if QUALIFIED == "Yes":
             INTEREST = Decimal('.0404')
         elif QUALIFIED == "No":
             INTEREST = Decimal('.0498')
     elif 20 <= YEARS <= 30:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
         if QUALIFIED == "Yes":
             INTEREST = Decimal('.0577')
         elif QUALIFIED == "No":
             INTEREST = Decimal('.0639')
 elif 200000 <= PRINCIPLE <= 999999:
-    YEARS = int(raw_input(
-    'For how many years is this loan being borrowed? '))
     if 1 <= YEARS < 15:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
         if QUALIFIED == "Yes":
             INTEREST = Decimal('.0302')
         elif QUALIFIED == "No":
             INTEREST = Decimal('.0398')
     elif 15 <= YEARS < 20:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
         if QUALIFIED == "Yes":
             INTEREST = Decimal('.0327')
         elif QUALIFIED == "No":
             INTEREST = Decimal('.0408')
-    elif 20 <= YEARS <= 30:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
-        if QUALIFIED == "Yes":
-            INTEREST = Decimal('.0466')
+    elif 20 <= YEARS <= 30 and QUALIFIED == "Yes":
+        INTEREST = Decimal('.0466')
 elif PRINCIPLE >= 1000000:
-    YEARS = int(raw_input(
-    'For how many years is this loan being borrowed? '))
-    if 1 <= YEARS < 15:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
-        if QUALIFIED == "Yes":
-            INTEREST = Decimal('.0205')
-    elif 15 <= YEARS <= 20:
-        QUALIFIED = raw_input("Are you prequalified for this loan? ").title()
-        if QUALIFIED == "Yes":
-            INTEREST = Decimal('.0262')
+    if 1 <= YEARS < 15 and QUALIFIED == "Yes":
+        INTEREST = Decimal('.0205')
+    elif 15 <= YEARS <= 20 and QUALIFIED == "Yes":
+        INTEREST = Decimal('.0262')
 
 TOTAL = (PRINCIPLE * (1+(INTEREST/1)) ** YEARS)
 REPORT = (
